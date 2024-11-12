e7b7159cd29335c91d795d89e5c7ccb425ba203c
RobertoArteaga25 <roberto.carlos.lopez@estudiante.uacm.edu.mx>
2024-11-12 17:05:02 -0600

    Interfaz ClienteRepo Lista

--- seguro-polizas/src/main/java/Repositorios/clienteRepositorio.java
+++ seguro-polizas/src/main/java/Repositorios/clienteRepositorio.java
@@ -1,5 +1,19 @@
 package Repositorios;
+import java.util.List;
 
-public class clienteRepositorio {
+import modelo.Cliente;
+import modelo.Poliza;
 
+
+
+public interface clienteRepositorio 
+{
+
+    public Cliente obtenerPorCurp(String curp);
+    public List<Cliente> obtenerTodos();
+    public Cliente guardar(Cliente cliente);
+    public Cliente actualizar(Cliente cliente);
+    public void eliminarPorCurp(String curp);
+    public List<Poliza> obtenerPolizasDeCliente(String curp);
+    public List<Cliente> buscarPorNombreOApellidos(String nombre, String Apellidos);
 }
